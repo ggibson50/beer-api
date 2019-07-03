e45e3ceb400b9344a95e6e2ba1b4e1e262a7dbd4
Geoffrey <geoff.gibson@daugherty.com>
2019-07-03 15:54:15 -0500

    routers

--- routes/beerRouter.js
+++ routes/beerRouter.js
@@ -1,5 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const Beer = require('../models/beer');
+
+router.post('/', (req, res) => {
+    let beer = new Beer();
+    beer.name = req.body.name;
+    beer.rating = req.body.rating;
+    beer.save((err, document) => {
+        if (err) {
+            res.status(400).send(err);
+        } else {
+            res.send(`Saved: ${document}`)
+        }
+    });
+});
+
+router.get('/', (req, res) => {
+    Beer.find((err, beers) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.json(beers)
+        }
+    })
+});
+
+router.get('/:beer_id', (req, res) => {
+    Beer.findById(req.params.beer_id, (err, beer) => {
+        if (err) res.send(err);
+        else res.json(beer);
+    }) 
+});
 
 router.get('/', (req, res) => {
     res.send('Router is functional')
--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const router = require('./routes/beerRouter');
 
-app.get('/beers', router);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/dogs', (req, res) => {
-    res.send(`Here's a dog!`);
+mongoose.connect('mongodb://localhost:27017/beers',  {
+    useNewUrlParser: true,
 });
 
-app.delete('/dogs', (req, res) => {
-    res.send(`Deleted a dog!`);
+mongoose.connection.on('connected', () => {
+    console.log('Connected to beers database');
 });
+
+mongoose.connection.on('error', (error) => {
+    console.log(`Error connecting to database:\n${error}`);
+});
+
+app.use('/beers', router);
 
 app.use('/', (req, res) => {
     res.send('Hello!');
